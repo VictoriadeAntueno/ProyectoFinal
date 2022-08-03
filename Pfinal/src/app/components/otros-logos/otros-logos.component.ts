fcaadeca68b4deb0751dfd8137d869a8929a54eb
VictoriadeAntueno <victoriadeantueno@gmail.com>
2022-08-03 10:35:42 -0300

    header

---
+++ Pfinal/src/app/components/otros-logos/otros-logos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-otros-logos',
+  templateUrl: './otros-logos.component.html',
+  styleUrls: ['./otros-logos.component.css']
+})
+export class OtrosLogosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
